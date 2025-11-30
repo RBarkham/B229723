5c9804dc050f6905923d7a0b363ac914732a68d3
Ryan Barkham <Ryan.Barkham@outlook.com>
2025-11-30 04:05:22 +0000

    Debug attempt 2 - editing iframe

--- Data/app.R
+++ Data/app.R
@@ -1,24 +1,24 @@
 
 library(shiny)
-library(ggplot2)
+library(ggplot2) # we want to reduce our data load for our server, so we won't load the whole tidyverse - just what we need.
 library(scales)
 library(sf)
 library(dplyr)
 
-Final_data_server <- readRDS("Final_data.rds")
-print("Server Final_data_server columns:")
-print(names(Final_data_server))#You would normally expect here() to start at the project working directory (B229723, however due to the nature of Shiny.io (where we host this app), here() actually starts us at Antibiotics_Maps.
+Final_data_server <- readRDS("Final_data_server.rds")
+#You would normally expect here() to start at the project working directory (B229723, however due to the nature of Shiny.io (where we host this app), here() actually starts us at Antibiotics_Maps.
 
 
 ui = fluidPage(
   titlePanel("Antibiotic Doses per Capita by Health Board"),
   sidebarLayout(
     sidebarPanel(
-      selectInput("Year", "Select Year", choices = c("2023", "2024")),
+      selectInput("Year", "Select Year", 
+                  choices = c("2023", "2024")),
       selectInput("Month", "Select Month", 
                   choices = c("jun", "jul", "aug", "sep", "oct", "nov", "dec"))),
     mainPanel(
-      plotOutput("antibiotic_map", height = "600px")
+      plotOutput("antibiotic_map", height = "80vh", width = "100%")
     )))
 
 server = function(input, output, session) {
